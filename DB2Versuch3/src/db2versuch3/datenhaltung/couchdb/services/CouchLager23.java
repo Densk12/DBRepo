7b9fd46de5818dacd7b5165869aed1579117fc01
Dennis Sk <Densk@gmx.de>
2018-06-26 13:42:39 +0200

    --

--- DB2Versuch3/src/db2versuch3/datenhaltung/couchdb/services/CouchLager23.java
+++ DB2Versuch3/src/db2versuch3/datenhaltung/couchdb/services/CouchLager23.java
@@ -1,10 +1,17 @@
 package db2versuch3.datenhaltung.couchdb.services;
 
+import db2versuch3.datenhaltung.couchdb.entites.Lager;
+import java.util.List;
+
 /**
  * Schnittstellenklasse CouchLager23
  * 
  * @version 1.0
  */
 public interface CouchLager23 {
-    
+    public abstract boolean lagerCSVEinfuegen();
+    public abstract List<Lager> lagerAllesLesen();
+    public abstract Lager lagerLesenById(int id);
+    public abstract boolean lagerUpdate(Lager lager);
+    public abstract boolean lagerDeleteById(int id);
 }
